c716947e8f8cd68a36b3a52f2b1c9997cb97392f
Brandon Yee <byee20@punahou.edu>
2017-05-06 23:50:12 -1000

    Updated
    EasyBib updated their page format, and as a result, this broke
    EasierBib. However, EasierBib is now fixed!

--- removeAds.js
+++ removeAds.js
@@ -1,9 +1,15 @@
 console.log("Removing Easybib ads using EasierBib!");
 
-$("#premium_ad").remove();
-
 var removeAds = setInterval(function () {
-	$("[data-type=ads-keep-us-free]").parent().parent().parent().each(function removeAds () {
+	$("iframe").each(function removeFlash () {
+		if ($(this).attr("id") != undefined && $(this).attr("id").indexOf("google_ads") > -1) {
+			$(this).remove();
+			console.log("Removed flash iframe " + $(this).prop("id"));
+		}
+	})
+
+	// .not("main") prevents the removal of the actual content
+	$("[data-type=ads-keep-us-free]").parent().parent().parent().not("main").each(function removeAds () {
 		$(this).remove();
 		console.log("Removed ad " + $(this).prop("id"));
 	});
